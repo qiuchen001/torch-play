a96b0aab854d11b145bab643e32d9cbc1d7b1abe
zhangchenchen <zhangchenchen@51aes.com>
2025-09-26 17:01:06 +0800

    增加LSTM

---
+++ Pytorch 深度学习实战/23 情感分析：如何使用LSTM进行情感分析？/test_code.py
@@ -0,0 +1,13 @@
+# 读取IMDB数据集
+import torchtext
+from torchtext import datasets
+# train_iter = torchtext.datasets.IMDB(root='./data', split=('train', 'test'))
+# train_iter = iter(train_iter)
+
+# next(train_iter)
+
+
+train_iter, test_iter = datasets.IMDB(root='data', split=('train', 'test'))
+train_iter = iter(train_iter)
+data = next(train_iter)
+print(data)
